07859b37ea9287ff8620e84580a00c12a2e731bd
Ken <kennethremigio@yahoo.com>
2023-09-19 14:13:55 +0800

    Dashboard changes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -10,14 +10,26 @@ const Dashboard = (props) => {
     return ( <>
     <div className="dashboard">
             <h1 className="userName">Welcome {user.Username}!,</h1>
+            {/* <div className='menuBtn'>
+                <button className='btnDash'><img src="src/components/Images/Dashboard.png" alt="imgLogo" id='dashboardLogo'/>Dashboard</button>
+                <button className='btnDash'><img src="src/components/Images/transferIcon.png" alt="imgLogo" id='dashboardLogo'/>Transfer</button>
+                <button>Transactions</button>
+                <button>Linked Cards</button>
+            </div> */}
             <div className="currentBalance">
-                <label>P0.00</label>
-                <p>Balance Amount</p>
+                <label id='balanceAmount'>P0.00</label>
+                <p id='balanceMessage'>Total Balance</p>
             </div>
-                <button className="addMoneyBtn">Add Money</button>
+            <div className='totalSavings'>
+                <label id='balanceAmount'>P0.00</label>
+                <p className='totalBalanceSavings'>Total Savings</p>
+            </div>
+           <button className="addMoneyBtn">Add Money</button>
+                <p className='transHistory'>Transaction History</p>
         
             <button className="sendMoney">Send Money</button>
             <button className="addCard">Add Card</button>
+
         
 
 
